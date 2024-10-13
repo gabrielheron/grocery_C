9fff346ba139044392911e15fe25b940cf3803f4
Heron <gabrielheron63@gmail.com>
2024-10-13 00:43:18 -0300

    cu

--- grocery.c
+++ grocery.c
@@ -1,105 +1,90 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
 
 #define MAX_LETRAS_PRODUTOS 10
-#define MAX_ESTOQUE_PRODUTOS 100
+#define MAX_ESTOQUE 100
+#define MAX_CARRINHO 50 
 
 /* --- Quitanda --- */
 
-/*
-   1 - Verificar as opcoes
-   2 - Escolher uma das opcoes apertando o numero determinado por ela
-   3 - Seguir o programa
-*/
-
-// Configuracoes padrao para os produtos
 struct produto {
-        int codigo;
-        char nome [MAX_LETRAS_PRODUTOS];
-        float preco;
-        int qnt [MAX_ESTOQUE_PRODUTOS];
-    };
-
-// typedef struct produto produto;
-
-// Gerador de codigo
-void produto_codigo(struct codigo cod[], int tam)
-{
-    for (i = 0; i < tam; i++)
-    {
-        cod[i].codigo = 1003 + i;
-        snprintf(cod[i].nome, sizeof(cod[i].nome), "O codigo do produto %d", cod[i].codigo);
-    };
+    int codigo;
+    int nome[MAX_LETRAS_PRODUTOS];
+    int preco;
+    int qnt;
 }
 
-void produto_exibir (struct produto produtos[], int est)
+void estoque_adicionar (struct produto estoque[], int *qnt_estoque) 
+// Um array 'estoque[]' que armazena os produtos da struct
+// Um ponteiro que '*qnt_estoque' modifica a variável diretamente na função   
 {
-    printf("Produtos em estoque:\n");
-    for (i = 0; i < est; i++){
-        printf("Codigo: %d, Nome: %s, Preco: %.2f, Quantidade: %d\n",
-               produtos[i].codigo, produtos[i].nome, produtos[i].preco, produtos[i].qnt)
+    if (qnt_estoque >= MAX_ESTOQUE) 
+    {
+        printf("Estoque cheio!\n");
+        return; // Se o estoque estiver cheio a função retorna 
     }
-}
 
-int produto_adicionar()
-{
-    for (i=0; i< o; i++)
-    {
-        printf("Qual o produto escolhido:\n");
-        scanf("%s", produtos[i].nome);
+    printf("Nome do produto:\n", MAX_LETRAS_PRODUTOS - 1);
+    scanf("%s", estoque[*qnt_estoque].nome);
 
-        printf("Qual a quantidade:\n");
-        scanf("%d", produtos[i].qnt );
-    }
-}
-
-void produto_remover(struct produto cod[]. int *est, int codigo)
-{
-    int found = -1;
-
-    // Achar o produto para remover
-    for (i = 0; i < *est; i++)
-    {
-    if(produtos[i].codigo == codigo )
+    do {
+        printf("Valor do produto:\n");
+        scanf("%.2f", estoque[*qnt_estoque].preco);
+        if (estoque[*qnt_estoque].preco < 0) 
         {
-        found = i;
-        break
+            printf("O preco nao pode ser negativo. Tente novamente.\n");
+        }
+    } while (estoque[*qnt_estoque].preco < 0);    
+    
+    do {
+        printf("A quantidade que vai para o estoque");
+        scanf("%d", estoque[*qnt_estoque].qnt);
+        if (estoque[*qnt_estoque].qnt < 0)
+        {
+            printf("A quantidade em estoque nao pode ser negativo. Tente novamente. \n");
+        }
+    } while (estoque[*qnt_estoque].qnt < 0);
+    
+    estoque[*qnt_estoque].codigo = 1000 + *qnt_estoque; 
+    printf("Produto %s foi adicionado com o codigo %d", estoque[*qnt_estoque].nome, estoque[*qnt_estoque].codigo);
+
+    (*qnt_estoque)++; 
+}
+
+void estoque_remover (struct produto estoque[], int *qnt_estoque, int codigo)
+{
+    int encontrar = -1;
+
+    for (i = 0; i < *qnt_estoque; i++)
+    {
+        if (estoque[i].codigo == codigo)
+        {
+            encontrar = i;
+            break;
         }
     }
 
-    // O produto encontrado sera removido
-    if (found != -1) {
-        for (int i = found; i < *est - 1; i++) {
-            produtos[i] = produtos[i + 1]; // Mover produtos para a esquerda
+    if (encontrar != -1)
+    {
+        for (int i = encontrar; i < *qnt_estoque; i++) 
+        {
+            estoque[i] = estoque[i + 1]; // Irá mover o elemento para a esquerda
         }
-        (*est)--; // Diminuir a quantidade de produtos
-        printf("Produto %s %d foi removido com sucesso!\n", produtos[i].nome, codigo);
+        (*qnt_estoque)--;
+        printf("Produto removido do estoque. \n");
     } else {
-        printf("Produto nao encontrado.\n");
+        printf("Produto nao encontrado. \n");
     }
 }
 
-void estoque_adicionar(struct produto )
+void estoque_verificar (struct produto estoque[], int qnt_estoque)
 {
-     printf("Indique o nome do produto:\n");
-        canf("%s", cod[i].nome);
-
-        printf("Indique o preco do Produto:\n");
-        scanf("%.2f", &cod[i].preco);
-        
-        printf("Indique q quantidade que vai estar em estoque:\n");
-        scanf("%d", &cod[i].qnt);
-        
-}
-
-//void estoque_remover()
-{
-}
-
-void estoque_verificar()
-{
+    if (qnt_estoque == 0)
+    {
+        printf("Estoque vazio! \n");
+        return;
+    }
     printf("printf             ***** ESTOQUE ***** \n");
     printf("---------------------------------------------------------
     ---------\n");
@@ -107,114 +92,165 @@ void estoque_verificar()
     printf("---------------------------------------------------------
     ---------\n")
     
-    for (i = 0; i < est; i++)
+    for (i = 0; i < qnt_estoque; i++)
     {
-        printf("%d %s %.2f %d \n", i+1, produtos[i].codigo, produtos[i].nome, produtos[i].preco, produtos[i].qnt);
+        printf("%d %s %.2f %d \n", i+1, estoque[i].codigo, estoque[i].nome, estoque[i].preco, estoque[i].qnt);
     }
 }
 
-void carrinho_verificar(){
+void carrinho_adicionar (struct produto estoque[], int qnt_produtos, struct produto carrinho[], int *qnt_carrinhos)
+{
+    int encontrar = -1;
 
+    if (qnt_carrinho >= MAX_ESTOQUE) 
+    {
+        printf("Carrinho cheio!\n");
+        return;
+    }
+
+    for (i = 0; i < qnt_estoque; i++) // Encontrar no estoque 
+    {
+        encontrar = i;
+        break;
+    }
+
+    if (encontrado == -1)
+    {
+        printf("O produto nao encontrado!");
+        return;
+    }
+
+    if (estoque[encontrar].qnt < quantity) 
+    {
+        printf("Não há quantidade o suficiente!");
+    }
+
+    // Adicionar ao carrinho 
+    carrinho[*qnt_estoque].nome = estoque[encontrar];
+    carrinho[*qnt_estoque].qnt = quantity;
+
+    // Diminuir o elemento no array 
+    estoque[encontrar].qnt -= quantidade;
+    
+    (*qnt_carrinho)++;
+    
+    printf("O produto foi adicionado ao carrinho!");    
 }
 
-float carrinho_soma(float *a, float *b){
-    float soma;
-    return soma = (a+b);
+void carrinho_remover (struct produto carrinho[], int *qnt_carrinho, int codigo)
+{
+     encontrado = -1;
+
+    for (i = 0; i < *qnt_carrinho; i++)
+    {
+        if (carrinho[i].codigo == codigo)
+        {
+            encontrado = i;
+            break;
+        }
+    }
+
+    if (encontrado != -1)
+    {
+        for (int i = encontrado; i < *qnt_carrinho; i++) 
+        {
+            carrinho[i] = estoque[i + 1]; // Irá mover o elemento para a esquerda
+        }
+
+        (*qnt_carrinho)--;
+        printf("Produto removido do estoque. \n");
+    } else {
+        printf("Produto nao encontrado. \n");
+    }
 }
 
-int main() {
-
-    int options;
-    printf("                    ----- QUITANDA-----\n ");
+void carrinho_verificar (struct produto carrinho[], int qnt_carrinho)
+{
+    if (qnt_carrinho == 0)
+    {
+        printf("Carrinho vazio! \n");
+        return;
+    }
+    printf("printf             ***** CARRINHO ***** \n");
     printf("---------------------------------------------------------
     ---------\n");
     printf("Numero|    NOME          |   CODIGO  |  QUANTIDADE |  PRECO \n");
     printf("---------------------------------------------------------
     ---------\n")
-    printf("")
-
-    do {
-
-        printf("Opcoes:
-        1 - Adicionar carrinho\n
-        2 - Remover carrinho\n
-        3 - Adicionar estoque\n
-        4 - Remover estoque\n
-        5 - Verificar estoque\n
-        6 - Verificar carrinho\n
-        7 - Comprar\n
-        8 - Sair\n
-        ");
-
-        scanf("%d", &options);
-
-            switch (options)
-            {
-                case 1:
-
-                    do {
-                    printf("Indique o nome do produto: ");
-                    scanf("%s", produtos[i].nome);
-                    } while ()
-
-
-                break;
-                case 2:
-                break;
-                case 3:
-                    
-
-                break;
-                case 4:
-                    printf("Qual o produto que ira remover (indique o seu codigo):\n");
-                    //scanf("%d", &cod[i].codigo);
-
-                    if (cod[i].codigo > 1000 && cod[i].codigo < 1011){
-                        produto_remover;
-                    } else {
-                        printf("Opcao invalida!");
-                    }
-                break;
-                case 5:
-                    printf("")
-                break;
-                case 6:
-                break;
-                case 7:
-                    printf("A compra sera realizada ...");
-                    printf("O preco total foi %.2f!", compra_soma);
-                    prinft("Comprar agora(1) ou pendurar(2) ?");
-                    scanf("%c", result);
-
-                    if (result == "1")
-                    { 
-                        printf("Compra realizada!");
-                        printf("Saindo...");
-                        getchar();   
-                    } else if (result == "2"){
-
-                    } else {
-                        printf("Opcao invalida!");
-                    }
-                break;
-                case 8:
-                    printf("Saindo...");
-                break;
-                default
-                printf("Opcao invalida!");
-                break;
-
-    } while (options != 8);
-
-    if (options != 8)
+    
+    for (i = 0; i < qnt_carrinho; i++)
     {
-        printf("Obrigado pela compra!");
-    } else {
-        printf("Compre na proxima vez");
+        printf("%d %s %.2f %d \n", i+1, estoque[i].codigo, estoque[i].nome, estoque[i].preco, estoque[i].qnt);
+    }
+}
+
+void carrinho_comprar (struct produto carrinho[], int *qnt_carrinho)
+{
+    float total = 0.0;
+    
+    if (*qnt_carrinho == 0) {
+        printf("Carrinho vazio.\n");
+        return;
+    }
+    
+    printf("A compra vai ser finalizada...\n");
+    
+    for (int i = 0; i < *qnt_carrinho; i++) {
+        total += carrinho[i].preco * carrinho[i].qnt;
+    }
+    
+    printf("Valor total da compra: R$%.2f\n", total);
+    printf("Compra realizada com sucesso!\n");
+    
+    // O carrinho vai ser esvaziado apos a compra
+    *qnt_carrinho = 0;
+}
+
+int main () 
+{
+    // Variáveis estoque
+    struct produto estoque[MAX_ESTOQUE];
+    int qnt_produtos = 0; // Declaração da variável que armazena os produtos
+
+    // Variáveis carrinho
+    struct produto carrinho[MAX_CARRINHO];
+    int qnt_carrinho = 0;
+
+    // Opções  
+    int options;
+    printf("Opcoes:\n"
+       "1 - Adicionar ao carrinho\n"
+       "2 - Remover do carrinho\n"
+       "3 - Adicionar ao estoque\n"
+       "4 - Remover do estoque\n"
+       "5 - Verificar estoque\n"
+       "6 - Verificar carrinho\n"
+       "7 - Comprar\n"
+       "8 - Sair\n");
+
+    switch (options) {
+        case 1:
+
+        break;
+        case 2:
+        break;
+        case 3:
+        break; 
+        case 4:
+        break; 
+        case 5:
+        break;
+        case 6:
+        break;
+        case 7:
+        break;
+        case 8:
+            printf("O programa sera fechado!");
+            printf("Saindo ...");
+        break;
+        default:
+            printf("Opcao invalida!");
+        break;
     }
 
-    printf("Pressione ENTER para sair!");
-    getchar();
-    
-    return 0;
 }
